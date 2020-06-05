35a8d32bf23580194df8aa64ec8a381e32493d4a
Oscar Alfredo  Cruz Rovira <cruzrovira@gmail.com>
2020-06-04 21:46:47 -0600

    Updates

---
+++ precache-manifest.ab119ab9ca3c2aee88923d269784fc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f2344f07f68751477a29abee1ee3a9",
+    "url": "/four-card-feature-section/index.html"
+  },
+  {
+    "revision": "61462cffe6067a74113c",
+    "url": "/four-card-feature-section/static/css/main.4b2c9354.chunk.css"
+  },
+  {
+    "revision": "9aa51ab8b67d873aebc9",
+    "url": "/four-card-feature-section/static/js/2.3ef3de57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/four-card-feature-section/static/js/2.3ef3de57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61462cffe6067a74113c",
+    "url": "/four-card-feature-section/static/js/main.85c64ae3.chunk.js"
+  },
+  {
+    "revision": "c657dd6858909b39b325",
+    "url": "/four-card-feature-section/static/js/runtime-main.cf5d0f15.js"
+  }
+]);
